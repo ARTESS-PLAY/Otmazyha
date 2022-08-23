a109c2fd5fb853fcb77e451978be31a3a1cdfbe5
Pan0mera <artess.ponomarev@mail.ru>
2022-08-23 17:39:01 +0300

    added backUrl in RulsPage

--- src/pages/RulsPage.jsx
+++ src/pages/RulsPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Wrapper from '../components/Wrapper';
+import BackUrl from '../components/UI/BackUrl/BackUrl';
 
 const RulsPage = () =>{
   return(
     <Wrapper>
+    <BackUrl>Назад</BackUrl>
       <div className='textRuls'>
         <h1>Правила игры</h1>
         <p>Игроки тянут карточку(одна на всех).
